ab4c62a4e3566e1140ea3eb9e6300fdbac4fb6e0
Alex Abenoja <abenojaalex@gmail.com>
2019-02-25 14:58:22 -0700

    Add basic setup using auth0 jwt

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+
+const app = express();
+
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://qragga-apps.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'https://faeron-hooks.herokuapp.com',
+  issuer: 'https://qragga-apps.auth0.com',
+  algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
+app.get('/authorized', (req, res) => {
+  res.send('Secured Resource');
+});
+
+const port = process.env.PORT || 3031;
+app.listen(port, () => {
+  console.log('hooks started');
+});
